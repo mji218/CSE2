a32a54adde39d21d48829b6f57a4412d171bcf3d
mji218 <mji218@lehigh.edu>
2015-01-23 20:21:22 +0000

    Homework 1

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,31 @@
+/////////////////////////////////////////
+//Michelle Iskandar
+//hw01
+//January 23 2014
+//WelcomeClass Java Program
+//
+//  first compile the program
+//     java WelcomeClass.java
+//  run the program
+//  java WelcomeClass//
+
+//define a class
+public class WelcomeClass{
+    
+//  add main method
+   public static void main(String[] args) {
+       
+//  Print the welcome statement
+      System.out.println("-----------");
+      System.out.println("| WELCOME |");
+      System.out.println("-----------");
+      System.out.println(" ^  ^  ^  ^  ^  ^");
+      System.out.println("/ \\/ \\/ \\/ \\/ \\/ \\");
+      System.out.println("<-M--J--I--2--1--8->");
+      System.out.println("\\ /\\ /\\ /\\ /\\ /\\ /");
+      System.out.println(" v  v  v  v  v  v ");
+      System.out.println("I was born and raised in Boston and I will be rooting for the Patriots during the Super Bowl!");
+      
+      
+   }
+}
